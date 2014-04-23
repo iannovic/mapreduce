bd12342f24f602e67e9dc71c6f5d3489fb455200
ian <ian@ian-VirtualBox>
2014-04-22 20:14:14 -0400

    fixed general wordcount

--- wordcount-sample/src/sample/TokenizerMapper.java
+++ wordcount-sample/src/sample/TokenizerMapper.java
@@ -13,15 +13,18 @@ public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
 	private Text word = new Text();
 
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		
 		StringTokenizer itr = new StringTokenizer(value.toString());
+		
 		while (itr.hasMoreTokens()) {
 			word.set(itr.nextToken());
 			String s = word.toString();
-			if (s.charAt(0) == '@' || s.charAt(0) == '#' || s.contains("http")) {
-			} else {
-				s = s.replaceAll("[.:,?!\"]", " ").toLowerCase().trim();	
-				word.set(s);
-				context.write(word, one);
+			if (!s.matches("[0-9]")) {
+				s = s.replaceAll("[^A-Za-z']","").toLowerCase().trim();	
+				if (!s.isEmpty()) {
+					word.set(s);
+					context.write(word, one);
+				}
 			}
 		}
 	}
