c4bb4a3fa76c53fa8b4b4bc7cbbc29ccff544e58
Kevin <kevinnea@buffalo.edu>
2014-04-25 15:15:16 -0400

    Fixed branch

--- wordcount-sample/src/sample/TokenizerMapper.java
+++ wordcount-sample/src/sample/TokenizerMapper.java
@@ -1,116 +1,63 @@
 package sample;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.retry.RetryPolicies.MultipleLinearRandomRetry.Pair;
 import org.apache.hadoop.mapreduce.Mapper;
 
-
-
-
-public class TokenizerMapper extends Mapper<Object, Text, TextPair, IntWritable>{
-
+public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+	/*
+	 * this version of the mapper will SORT any previous wordcount based on the value.
+	 * the idea is simple; we concanate the value of the <key,value> onto the front of the key.
+	 * the new key is made to be a concanated 
+	 * 			string {oldvalue + " " + oldkey} = newkey
+	 * mapreduce will automatically sort these keys to be in increasing order by value.
+	 * 
+	 * we had to add one work around to this code, and that was to append zeroes to each value until they all
+	 * were of the same size, we chose the number 7 because we will never run a mapreduce job of more than 100,000 tweets
+	 * so a maximum of 7 digits suits the size of our jobs, a word will never appear more than one million times.
+	 */
 	private final static IntWritable one = new IntWritable(1);
 	private Text word = new Text();
-	private final int WORD = 0;
-	private final int HASH = 1;
-	private final int MENTION =2;
+	//change this variable to get the desired output
+	int mode = 0;
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		
 		StringTokenizer itr = new StringTokenizer(value.toString());
-		int mode = 0;
-		ArrayList<String> wordList = new ArrayList<String>();
-		int c = 0;
-		whileLoop:
+		
 		while (itr.hasMoreTokens()) {
 			word.set(itr.nextToken());
 			String s = word.toString();
-				switch(c){
-				case(0):
-					if(s.equals("Sun") || s.equals("Mon") || s.equals("Tue")
-							|| s.equals("Wed") || s.equals("Thu") || s.equals("Fri")
-							|| s.equals("Sat")){
-						c = 1;
-					}
-				break;
-				case(1):
-					if(s.equals("Apr")){
-						c++;
-					}else{
-						if(s.equals("Sun") || s.equals("Mon") || s.equals("Tue")
-								|| s.equals("Wed") || s.equals("Thu") || s.equals("Fri")
-								|| s.equals("Sat")){
-							c = 1;
-						}else{c=0;}
-					}
-				break;
-				case(2):
-					if(s.contains("0") || s.contains("1") || s.contains("2") || s.contains("3")){
-						c = 0;
-						for(int i = 0; i < 2; i++){wordList.remove(wordList.size() - 1);}
-						for(int i = 0; i < 5; i++){
-							if(itr.hasMoreTokens()){
-								itr.nextToken();
-							}else{break whileLoop;}
-						}
-						write(wordList, context);
-						wordList.clear();
-					}else{
-						if(s.equals("Sun") || s.equals("Mon") || s.equals("Tue")
-								|| s.equals("Wed") || s.equals("Thu") || s.equals("Fri")
-								|| s.equals("Sat")){
-							c = 1;
-						}else{c=0;}
-					}
-				break;
-				default:
-				break;
-			}
+			
+			//this regex will match string s to be one of the original key's, 
+			//because the new input to this mapreduce is both keys and values of a previous mapreduce.
+			//once we have made a match, we need to iterate the white space until we have the value that matched with the key
+			//and once we have that key,value, we must append the value until it has a uniform amount of digits as other values.
+			
 			switch(mode){
-				case(WORD):
+				case(0):
 					if (s.charAt(0) == '@' || s.charAt(0) == '#' || s.contains("http")) {
 					} else {
 						s = s.replaceAll("[-0-9.:;\n\t\r#@, +?/!$&)(\"]*", "").toLowerCase().trim();	
-						if(s != "" && s != null){wordList.add(s);}
+						if(s != "" && s != null){context.write(word, one);}
 					}
 				break;
-				case(HASH):
+				case(1):
 					if (s.charAt(0) == '#'){
-						wordList.add(s);
+						context.write(word, one);
 					}
 					break;
-				case(MENTION):
+				case(2):
 					if (s.charAt(0) == 'a'){
-						wordList.add(s);
+						context.write(word, one);
 					}
 					break;
 				default:
 					break;
-				}	
-		}
-		write(wordList, context);
-	}
-	
-	private void write(ArrayList<String> wordList, Context context) {
-		
-		for(int i = 0; i < wordList.size() - 1; i++){
-			for(int j = 0; j < wordList.size() - 1; j++){
-				if(!(i==j) && wordList.get(i) != null && wordList.get(j) != null && wordList.get(i) != "" && wordList.get(j) != ""){
-					String first = wordList.get(i);
-					String second = wordList.get(j);
-					try {
-						context.write(new TextPair(first,second), one);
-						context.write(new TextPair(first,"*"), one);
-					} catch (IOException | InterruptedException e) {
-						e.printStackTrace();
-					}
 				}
+				
 			}
-		}
 	}
 }
-
-
